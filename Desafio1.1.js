134d535cfb763684cae905eeaf5eb75a388f7520
Airton Gamarra <airtongamarra575@gmail.com>
2022-10-30 10:48:14 -0300

    Repositorio

---
+++ Desafio1.1.js
@@ -0,0 +1,7 @@
+//1.1 Faça um algoritmo que receba um valor, inteiro e positivo, e usando a estrutura FOR exiba no console uma contagem de 0 até o número recebido.
+
+
+var numero = parseInt(prompt(" Insira um valor"))
+for(var contador = 0; contador <= numero; contador++){
+    console.log(contador)
+}
